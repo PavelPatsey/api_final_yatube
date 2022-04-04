444859c0e39c8750bce04a6e1c19918d4ecd174a
Павел Пацей <patsey.pavel@gmail.com>
2022-04-04 20:35:13 +0300

    black and osort tests/test_auth_jwt.py

--- yatube_api/api/tests/test_auth_jwt.py
+++ yatube_api/api/tests/test_auth_jwt.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
+from posts.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
-
-from posts.models import User
 
 
 class AuthJWTTest(TestCase):

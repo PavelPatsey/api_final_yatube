0d370ea370421f60bcb13b59fd90e2fe6cf5906b
Павел Пацей <patsey.pavel@gmail.com>
2022-04-05 11:06:30 +0300

    для PostViewSet и CommentViewSet в пермишеназ сделал IsAuthenticatedOrReadOnly

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from posts.models import Post
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, PostSerializer
@@ -10,7 +10,7 @@ from .serializers import CommentSerializer, PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated & IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -18,7 +18,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated & IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get("post_id")
